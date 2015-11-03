533d3472eebc5eb77f210326445f83de90c71cb9
skar.Wei <dtsps.skar@gmail.com>
2015-11-03 15:13:08 -0500

    commit with scratch

--- include/darray.h
+++ include/darray.h
@@ -1,2 +1,59 @@
+#ifndef _DARRAY_H_
+#define _DARRAY_H_
+
 #include <stdlib.h>
 #include <string.h>
+#include 
+
+#ifdef DEBUG
+
+#define darray_log_err(M, ...) \
+	fprintf(stderr, "[ERROR] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+
+#define darray_log_info(M, ...) \
+	fprintf(stderr, "[INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+
+#define check(A, M, ...) \
+	do{ \
+		if(!(A)) { \
+			log_err(M, ##__VA_ARGS__); \
+			errno=0; \
+			goto error; \
+		} \
+	}while(0)
+
+#endif
+
+
+#define _darray_type(T) \
+	struct { size_t element_size; size_t capacity; size_t length; T* content; }
+
+typedef struct _d_array
+{
+	/* data */
+	const size_t element_size;
+	size_t capacity;
+	size_t length;
+	char* content;		// TODO: figure out char* or char** works better
+} DArray;
+
+
+
+DArray *darray_create(size_t reserve_size, size_t element_size);
+
+bool _darray_reserve(DArray *arr, size_t n);
+
+bool _darray_expand(DArray *arr);
+
+void* _darray_get(DArray *arr, size_t index);
+
+void* _darray_set(DArray *arr, size_t index);
+
+typedef _darray_type(char) 		DArray_CHAR;
+typedef _darray_type(int) 		DArray_INT;
+typedef _darray_type(long) 		DArray_LONG;
+typedef _darray_type(float) 	DArray_FLOAT;
+typedef _darray_type(double) 	DArray_DOUBLE;
+
+#endif /* _DARRAY_H_ */
+
--- src/darray.c
+++ src/darray.c
@@ -1,1 +1,52 @@
 #include "darray.h"
+
+DArray *darray_create(size_t reserve_size, size_t element_size) {
+	DArray *arr = malloc(sizeof(DArray));
+	if (NULL == arr) {
+		log_err("Out of memory");
+		return NULL;
+	}
+	arr->capacity = 0;
+	arr->length = 0;
+	arr->element_size = element_size;
+	arr->content = NULL;
+	_darray_reserve(arr, reserve_size);
+	return arr;
+}
+
+bool _darray_reserve(DArray *arr, size_t n) {
+	if (n <= arr->capacity) {
+		return false;
+	}
+	size_t size = 1024;
+	while (size < n) size <<= 1;
+	void *tmp_ptr = realloc(arr->content, size * arr->element_size);
+	if (NULL == tmp_ptr) {
+		log_err("Out of memory");
+		return false;
+	}
+	arr->content = tmp_ptr;
+	arr->capacity = size;
+}
+
+bool _darray_expand(DArray *arr)
+{
+	if (arr->length + 1 > arr->capacity) {
+		void *ptr = NULL;
+		int n = (arr->capacity == 0)? 1024: (arr->capacity) << 1;
+		ptr = realloc(arr->content, n * element_size);
+		if (NULL == ptr) return false;
+		arr->content = ptr;
+		arr->capacity = n;
+	}
+	return true;
+}
+
+void *_darray_get(DArray *arr, size_t index) {
+	check(index < arr->length, "Attempt to get invalid data");
+
+}
+
+void *_darray_set(DArray *arr, size_t index) {
+	check(index < arr->length, "Attempt to set invalid data");
+}
